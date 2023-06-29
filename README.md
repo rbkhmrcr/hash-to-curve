08483704fd052c5867feb636773f8d7c20909fe3
rebekah <github@rbkhmrcr.com>
2023-06-29 13:19:14 +0200

    all

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# hash-to-curve
-Indifferentiable hashing to elliptic curves
+# Hashing to Elliptic Curves
+
+![Rustico](https://github.com/armfazh/h2c-rust-ref/workflows/Rustico/badge.svg)
+
+---
+
+**IETF Data Tracker**: [draft-irtf-cfrg-hash-to-curve](https://datatracker.ietf.org/doc/draft-irtf-cfrg-hash-to-curve)
+
+**Internet-Draft**: [git repository](https://github.com/cfrg/draft-irtf-cfrg-hash-to-curve)
+
+This document specifies a number of algorithms that may be used to encode or hash an arbitrary string to a point on an elliptic curve.
+
+### Reference Implementation
+
+The purpose of this implementation is for generating test vectors and enabling cross compatibility with other implementations.
+
+### Warning
+
+This implementation is **not** protected against any kind of attack,
+including side-channel attacks. It **MUST NOT** be used in production systems.
+
+**Limitations**
+-   No specific architecture optimizations.
+-   No side-channel protection, see [Warning](#Warning) section.
+
+**Development branch** [master](https://github.com/armfazh/h2c-rust-ref/tree/master)
+
+#### Draft versions implemented
+ Latest: [v14]
+
+ Previous: [v12], [v08], [v07], [v06], [v05]
+
+ [v14]: https://github.com/armfazh/h2c-rust-ref/tree/v14.0.0
+ [v12]: https://github.com/armfazh/h2c-rust-ref/tree/v12.0.0
+ [v08]: https://github.com/armfazh/h2c-rust-ref/tree/v8.0.0
+ [v07]: https://github.com/armfazh/h2c-rust-ref/tree/v7.0.0
+ [v06]: https://github.com/armfazh/h2c-rust-ref/tree/v6.0.0
+ [v05]: https://github.com/armfazh/h2c-rust-ref/tree/v5.0.0
+
+#### Compatible Implementations
+ -   [Sage](https://github.com/cfrg/draft-irtf-cfrg-hash-to-curve/tree/master/poc)
+ -   [Go](https://github.com/armfazh/h2c-go-ref)
+
+### Internals
+
+![hash to curve](https://github.com/cfrg/draft-irtf-cfrg-hash-to-curve/blob/main/drawings/diag.png)
+
+### License
+
+BSD 3-Clause License
+
+### Contact
+
+Feel free to open a github issue for anything related to the implementation, otherwise [e-mail](mailto:draft-irtf-cfrg-hash-to-curve@ietf.org) authors of the draft.
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,61 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+//! `h2c_rust_ref` is a reference implementation of hash to curve methods.
+//!
+//! # Warning
+//!
+//! This implementation is **not** protected against any kind of attack, including
+//! side-channel attacks. Do not use this code for securing any application.
+//!
+//! # Hash to Curve
+//!
+//! ```
+//!  use h2c_rust_ref::GetHashToCurve;
+//!  use h2c_rust_ref::{EDWARDS25519_XMDSHA512_ELL2_RO_,P256_XMDSHA256_SSWU_RO_};
+//!  let msg = b"Message string";
+//!  let dst = b"Domain separation tag";
+//!
+//!  let suite = P256_XMDSHA256_SSWU_RO_;
+//!  let h = suite.get(dst);
+//!  let mut p = h.hash(msg);
+//!  p.normalize();
+//!  println!("enc: {} {}", suite, p);
+//!
+//!  let suite = EDWARDS25519_XMDSHA512_ELL2_RO_;
+//!  let h = suite.get(dst);
+//!  let mut p = h.hash(msg);
+//!  p.normalize();
+//!  println!("enc: {} {}", suite, p);
+//! ```
+//!
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[macro_use]
+extern crate lazy_static;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod macros;
+
+mod api;
+mod edw;
+mod expander;
+mod fp;
+mod mont;
+mod weier;
+
+pub use crate::api::{GetHashToCurve, HashToCurve, HashToField, Suite};
+pub use crate::edw::{
+    EDWARDS25519_XMDSHA256_ELL2_NU_, EDWARDS25519_XMDSHA256_ELL2_RO_,
+    EDWARDS25519_XMDSHA512_ELL2_NU_, EDWARDS25519_XMDSHA512_ELL2_RO_,
+    EDWARDS448_XOFSHAKE256_ELL2_NU_, EDWARDS448_XOFSHAKE256_ELL2_RO_, SUITES_EDWARDS,
+};
+pub use crate::mont::{
+    CURVE25519_XMDSHA256_ELL2_NU_, CURVE25519_XMDSHA256_ELL2_RO_, CURVE25519_XMDSHA512_ELL2_NU_,
+    CURVE25519_XMDSHA512_ELL2_RO_, CURVE448_XOFSHAKE256_ELL2_NU_, CURVE448_XOFSHAKE256_ELL2_RO_,
+    SUITES_MONTGOMERY,
+};
+pub use crate::weier::{
+    BLS12381G1_XMDSHA256_SSWU_NU_, BLS12381G1_XMDSHA256_SSWU_RO_, BLS12381G1_XMDSHA256_SVDW_NU_,
+    BLS12381G1_XMDSHA256_SVDW_RO_, P256_XMDSHA256_SSWU_NU_, P256_XMDSHA256_SSWU_RO_,
+    P256_XMDSHA256_SVDW_NU_, P256_XMDSHA256_SVDW_RO_, P384_XMDSHA384_SSWU_NU_,
+    P384_XMDSHA384_SSWU_RO_, P384_XMDSHA384_SVDW_NU_, P384_XMDSHA384_SVDW_RO_,
+    P521_XMDSHA512_SSWU_NU_, P521_XMDSHA512_SSWU_RO_, P521_XMDSHA512_SVDW_NU_,
+    P521_XMDSHA512_SVDW_RO_, SECP256K1_XMDSHA256_SSWU_NU_, SECP256K1_XMDSHA256_SSWU_RO_,
+    SECP256K1_XMDSHA256_SVDW_NU_, SECP256K1_XMDSHA256_SVDW_RO_, SUITES_WEIERSTRASS,
+};
